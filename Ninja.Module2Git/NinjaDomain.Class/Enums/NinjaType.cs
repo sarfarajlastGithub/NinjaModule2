d631bd2e02cf31149ae993fa316f97241671ce1a
sarfarajlastGithub <sarfarajlast@gmail.com>
2016-12-02 00:16:41 +0530

    Frist Commit

---
+++ Ninja.Module2Git/NinjaDomain.Class/Enums/NinjaType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaDomain.Class
+{
+    public enum NinjaType
+    {
+        Shinobi = 1,
+        Kunoichi = 2
+
+    }
+}
---
+++ Ninja.Module2Git/NinjaDomain.DataModel/NinjaContext.cs
@@ -0,0 +1,17 @@
+﻿using NinjaDomain.Class;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaDomain.DataModel
+{
+    public class NinjaContext : DbContext
+    {
+        DbSet<Ninja> Ninjas { get; set; }
+        DbSet<Clan> Clans { get; set; }
+        DbSet<NinjaEquipment> Equipment { get; set; }
+    }
+}
